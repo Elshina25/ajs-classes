d3f3d4da0c211b3590bf2aeb518a46c5984685c6
Анастасия Ельшина <salnaya92@gmail.com>
2023-03-14 13:19:21 +0300

    edits made

--- src/js/Character.js
+++ src/js/Character.js
@@ -7,25 +7,4 @@ export default class Character {
     this.attack = attack;
     this.defence = defence;
   }
-
-  levelUp() {
-    const updateData = {
-      health: 100,
-      level: this.level += 1,
-      attack: (this.attack * 20) / 100 + this.attack,
-      defence: (this.defence * 20) / 100 + this.defence,
-    };
-    if (this.health > 0) {
-      return Object.assign(this, updateData);
-    }
-    throw new Error('Потрачено!');
-  }
-
-  damage(points) {
-    if (this.health > 0) {
-      this.health -= points * (1 - this.defence / 100);
-    } else {
-      throw new Error('Потрачено!');
-    }
-  }
 }
